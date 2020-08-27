277fbcd54f632dac0a6bf3966beec53597e8b3c0
Igor Sutton Lopes <isuttonl@redhat.com>
2020-08-27 11:52:46 +0200

    refactor: :recycle: move types from binding.go to definition.go

--- pkg/controller/servicebindingrequest/binding/binding.go
+++ pkg/controller/servicebindingrequest/binding/binding.go
@@ -1,32 +1,5 @@
 package binding
 
-const (
-	// configMapObjectType indicates the path contains a name for a ConfigMap containing the binding
-	// data.
-	configMapObjectType objectType = "ConfigMap"
-	// secretObjectType indicates the path contains a name for a Secret containing the binding data.
-	secretObjectType objectType = "Secret"
-	// stringObjectType indicates the path contains a value string.
-	stringObjectType objectType = "string"
-	// emptyObjectType is used as default value when the objectType key is present in the string
-	// provided by the user but no value has been provided; can be used by the user to force the
-	// system to use the default objectType.
-	emptyObjectType objectType = ""
-
-	// mapElementType indicates the value found at path is a map[string]interface{}.
-	mapElementType elementType = "map"
-	// sliceOfMapsElementType indicates the value found at path is a slice of maps.
-	sliceOfMapsElementType elementType = "sliceOfMaps"
-	// sliceOfStringsElementType indicates the value found at path is a slice of strings.
-	sliceOfStringsElementType elementType = "sliceOfStrings"
-	// stringElementType indicates the value found at path is a string.
-	stringElementType elementType = "string"
-)
-
-type objectType string
-
-type elementType string
-
 type ProjectionMapper interface {
 	Map([]Value) ([]Projection, error)
 }
